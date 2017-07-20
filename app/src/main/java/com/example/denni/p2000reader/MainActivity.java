25c22b53dbbea7df545ed9e6cc13203da1238758
Dennis Rump <dennisrump10@hotmail.com>
2017-07-20 23:23:30 +0200

    Second Commit

--- app/src/main/java/com/example/denni/p2000reader/MainActivity.java
+++ app/src/main/java/com/example/denni/p2000reader/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.denni.p2000reader;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void addRssFragment() {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        RssFragment fragment = new RssFragment();
+        transaction.add(R.id.fragment_container, fragment);
+        transaction.commit();
     }
 }
